1094d0572ab7954a53cbf94a45e19472c2bb8141
Amit Yadav <amit09yadav12@gmail.com>
2023-03-13 20:20:51 +0530

    6 push

--- config/db.js
+++ config/db.js
@@ -4,12 +4,11 @@ const connectWithDb = () => {
     mongoose.connect(process.env.DB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        console.log('conneted')
     })
     .then(console.log(`DB Got Connected`))
     .catch(error => {
         console.log(`DB CONNECTION ISSUE`);
-        console.log(error);
+        //console.log(error);
         process.exit(1);
     })
 }
